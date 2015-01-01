8014c05b42f244770a927d26989433aede3b3ca2
Alex Grabanski <ajg137@case.edu>
2015-01-01 12:44:55 -0500

    Added parse tree visitor and node class
    Signed-off-by: Alex Grabanski <ajg137@case.edu>

---
+++ src/main/java/Node.java
@@ -0,0 +1,62 @@
+package phi;
+import java.util.*;
+
+public class Node {
+    //For now, just contain a string as the name
+    public String label = "";
+    public ArrayList<Node> children = new ArrayList<Node>();
+    public Node(String label) {
+        this.label = label;
+    }
+    public Node add(Node child) {
+        this.children.add(child);
+        return this;
+    }
+
+    //Helper method to convert to the serialization format
+    private String serializeRecursive(String in) {
+        in += children.size() + ":" + label;
+
+        in += "\n";
+
+        for (int i=0; i < children.size(); i++) {
+            if (children.get(i) != null) {
+                in = children.get(i).serializeRecursive(in);
+            }
+        }
+        return in;
+    }
+    
+    public String toString() {
+        String result = serializeRecursive("");
+        if (result.length() == 0) {
+            return "";
+        }
+        //Lop off trailing newline
+        return result.substring(0, result.length() - 1);
+    }
+    
+    //Define equality on Nodes for convenience in testing
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Node) {
+            return this.equals((Node)obj);
+        }
+        return false;
+    }
+
+    public boolean equals(Node other) {
+        if (!this.label.equals(other.label)) {
+            return false;
+        }
+        if (other.children.size() != this.children.size()) {
+            return false;
+        }
+        for (int i=0; i < this.children.size(); i++) {
+            if (!this.children.get(i).equals(other.children.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/PhiParseVisitor.java
@@ -0,0 +1,110 @@
+package phi;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.ParserRuleContext;
+
+
+import phi.PHIParser;
+import phi.Node;
+import java.util.*;
+
+public class PhiParseVisitor extends AbstractParseTreeVisitor<Node>
+                    implements PHIVisitor<Node> {
+        
+    //Helper that takes a list of arguments and a label, and produces
+    //(label arg1 arg2 arg3 arg4...), where all arguments are visited
+    private <T extends ParserRuleContext> Node topLevelify(List<T> list, String label) {
+        Node result = new Node(label);
+        for (T item : list) {
+            result.add(visit(item));
+        }
+        return result;
+    }
+
+    @Override
+    public Node visitStart(PHIParser.StartContext ctxt) {
+        return visit(ctxt.blocklines());
+    }
+    @Override
+    public Node visitBlocklines(PHIParser.BlocklinesContext ctxt) {
+        return topLevelify(ctxt.getRuleContexts(PHIParser.BlocklineContext.class), "Expr");
+    }
+    @Override
+    public Node visitBlockline(PHIParser.BlocklineContext ctxt) {
+        //Blockline just passes right through
+        return visit(ctxt.getRuleContext(PHIParser.BlockitemsContext.class, 0));
+    }
+    @Override
+    public Node visitBlockitems(PHIParser.BlockitemsContext ctxt) {
+        //Just passes right through
+        return visit(ctxt.getRuleContext(ParserRuleContext.class, 0));
+    }
+    @Override
+    public Node visitBlockapp(PHIParser.BlockappContext ctxt) {
+        Node id = new Node(ctxt.ID().getText());
+        Node header = visit(ctxt.listitems());
+        Node body = visit(ctxt.blocklines());
+        //TODO: actually format this right!
+        Node result = new Node("BlockApp");
+        return result.add(id).add(header).add(body);
+    }
+    @Override
+    public Node visitListitems(PHIParser.ListitemsContext ctxt) {
+        return topLevelify(ctxt.getRuleContexts(PHIParser.ListitemContext.class), "Expr");
+    }
+    @Override
+    public Node visitListitem(PHIParser.ListitemContext ctxt) {
+        //Just passthrough
+        return visit(ctxt.getRuleContext(ParserRuleContext.class, 0));
+    }
+    @Override
+    public Node visitDotapp(PHIParser.DotappContext ctxt) {
+        //TODO: Actually implement this!
+        return topLevelify(ctxt.getRuleContexts(PHIParser.DotitemContext.class), "DotApp");
+    }
+    @Override
+    public Node visitDotitem(PHIParser.DotitemContext ctxt) {
+        return visit(ctxt.getRuleContext(ParserRuleContext.class, 0));
+    }
+    @Override
+    public Node visitItem(PHIParser.ItemContext ctxt) {
+        return new Node(ctxt.getText());
+    }
+    @Override
+    public Node visitFunapp(PHIParser.FunappContext ctxt) {
+        //TODO: actually implement this!
+        Node id = new Node(ctxt.ID().getText());
+        Node funitems = visit(ctxt.funitems());
+        return new Node("FunApp").add(id).add(funitems);
+    }
+    @Override
+    //TODO: Actually implement this!
+    public Node visitFunitems(PHIParser.FunitemsContext ctxt) {
+        return topLevelify(ctxt.getRuleContexts(PHIParser.ApaddeditemsContext.class), "Expr");
+    }
+    @Override
+    public Node visitApaddeditems(PHIParser.ApaddeditemsContext ctxt) {
+        return visit(ctxt.getRuleContext(PHIParser.AlistitemsContext.class, 0));
+    }
+    @Override
+    public Node visitAlistitems(PHIParser.AlistitemsContext ctxt) {
+        return topLevelify(ctxt.getRuleContexts(PHIParser.ListitemContext.class), "Expr");
+    }
+    @Override
+    public Node visitSexpr(PHIParser.SexprContext ctxt) {
+        return visit(ctxt.getRuleContext(PHIParser.ApaddeditemsContext.class, 0));
+    }
+    //TODO: elim in some way
+    @Override
+    public Node visitAspace(PHIParser.AspaceContext ctxt) {
+        return null;
+    }
+    @Override
+    public Node visitOptspace(PHIParser.OptspaceContext ctxt) {
+        return null;
+    }
+}
